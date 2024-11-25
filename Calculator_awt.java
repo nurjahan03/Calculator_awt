76e629695d7f83e9214cfe71271faa04f7a1db39
nurjahan03 <nurjahan0787@gmail.com>
2024-11-25 12:24:18 +0600

    1st version

---
+++ Calculator_awt.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator_awt;
+
+/**
+ *
+ * @author USER
+ */
+public class Calculator_awt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+    
+}
